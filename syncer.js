f53e03fba683922a971cdb889521b4a3c7a3e073
ducksquaddd <ducksquadddd@gmail.com>
2024-02-25 15:25:48 -0800

    Remove unused func

--- syncer.js
+++ syncer.js
@@ -311,26 +311,6 @@ module.exports = async function startSyncLoop() {
   }, 10000);
 };
 
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex > 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-}
-
 function deterministicSortAndShuffle(array, seed) {
   // console.log(array)
   const prng = seedrandom(seed);
